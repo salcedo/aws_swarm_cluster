6e5e4f7c53f5aaa47c275139e27a0ba9dc4ebfe3
Brian Salcedo <brian.salcedo@cleverthis.com>
2025-07-13 21:50:50 -0500

    add InstanceType tags

--- worker_pool/instance.tf
+++ worker_pool/instance.tf
@@ -29,6 +29,7 @@ resource "aws_instance" "worker" {
     tags = merge(var.tags, {
       Instance         = "${var.cluster_name}-${var.environment}-wkr-${var.name}-${var.availability_zones[count.index % length(var.availability_zones)]}${count.index + 1}"
       AvailabilityZone = "${var.region}${var.availability_zones[count.index % length(var.availability_zones)]}"
+      InstanceType     = var.instance_type
     })
     volume_size = var.root_volume_size
     volume_type = "gp3"
@@ -39,6 +40,7 @@ resource "aws_instance" "worker" {
   tags = merge(var.tags, {
     Name             = "${var.cluster_name}-${var.environment}-wkr-${var.name}-${var.availability_zones[count.index % length(var.availability_zones)]}${count.index + 1}"
     AvailabilityZone = "${var.region}${var.availability_zones[count.index % length(var.availability_zones)]}"
+    InstanceType     = var.instance_type
   })
 
   user_data = <<-EOF
